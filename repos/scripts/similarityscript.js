a5ea35d297d97218208b2a346fe8729f0d83c3b2
ceresbakalite <ceres@users.noreply.github.com>
2020-08-28 04:28:04 +1000

    Update scripts

--- repos/scripts/similarityscript.js
+++ repos/scripts/similarityscript.js
@@ -1,12 +1,30 @@
-function getMarkdown(ms)
+function getMarkdown(ms, target)
 {
-  refreshMarkdown_Shell();
+  switch (target)
+  {
+    case 'shell':
+      refreshMarkdown_Shell();
+      break;
+
+    case 'repos':
+      refreshMarkdown_Repos();
+      break;
+
+    default:
+      refreshMarkdown_Shell();
+  }
+
   var interval = setInterval(waitForMarkdown, ms);
 }
 
 function refreshMarkdown_Shell()
 {
   document.getElementsByTagName("zero-md")[0].setAttribute("file", "https://ceresbakalite.github.io/similarity/shell/README.md?" + getRandomInteger(10000,1000000));
+}
+
+function refreshMarkdown_Repos()
+{
+  document.getElementsByTagName("zero-md")[0].setAttribute("file", "https://ceresbakalite.github.io/similarity/README.md?" + getRandomInteger(10000,1000000));
 }
 
 function waitForMarkdown()
