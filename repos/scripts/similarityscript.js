eb4bb013af11bb996285007f4b79994e58d98eb8
ceresbakalite <ceres@users.noreply.github.com>
2020-08-28 07:28:08 +1000

    Update scripts

--- repos/scripts/similarityscript.js
+++ repos/scripts/similarityscript.js
@@ -26,7 +26,6 @@ function refreshMarkdown_Repos()
 {
   document.getElementById("md-logo").setAttribute("src", "https://ceresbakalite.github.io/similarity/images/NAVSimilarityLogoShell.png");
   document.getElementByTagName("zero-md")[0].setAttribute("file", "https://ceresbakalite.github.io/similarity/README.md?" + getRandomInteger(10000,1000000));
-  document.getElementsByTagName("zero-md")[0].style.display = "block";
 }
 
 function waitForMarkdown()
