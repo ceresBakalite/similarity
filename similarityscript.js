eb54326be3179acc476007f2113acc2269fd1778
ceresbakalite <ceres@users.noreply.github.com>
2020-08-27 19:51:43 +1000

    Create similarityscript.js

---
+++ similarityscript.js
@@ -0,0 +1,10 @@
+function markdownpause(ms)
+{
+  var interval = setInterval(waitformarkdown, ms);
+}
+
+function waitformarkdown()
+{
+  document.getElementById("zero-md-display").style.display = "block";
+  document.getElementById("site-footer-display").style.display = "block";
+}
