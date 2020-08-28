250581a69d942ee1d0ae7b82487dbc4dd86249b9
ceresbakalite <ceres@users.noreply.github.com>
2020-08-29 04:03:13 +1000

    Update similarityscript.js

--- repos/scripts/similarityscript.js
+++ repos/scripts/similarityscript.js
@@ -44,6 +44,6 @@ function getRandomInteger(min, max)
 function resetMarkdown_Logo(url)
 {
   alert('resetMarkdown_Logo');
-  document.getElementsByTagName("img")[0].setAttribute("src", url);
+  document.getElementById("logo-default").setAttribute("src", url);
   alert('end resetMarkdown_Logo');
 }
