bafa5a96792941bbad76eca61107876e0b98d37c
ceresbakalite <ceres@users.noreply.github.com>
2020-08-29 01:20:32 +1000

    Update zero-md

--- repos/scripts/similarityscript.js
+++ repos/scripts/similarityscript.js
@@ -43,13 +43,3 @@ function getRandomInteger(min, max)
 {
   return Math.floor(Math.random() * (max - min) ) + min;
 }
-
-Object.prototype.isEmpty = function()
-{
-    for (var key in this)
-    {
-        if(this.hasOwnProperty(key)) return false;
-    }
-
-    return true;
-}
