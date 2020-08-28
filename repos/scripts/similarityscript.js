71ff2c68564175be81b934e10c5ba80dd966811d
ceresbakalite <ceres@users.noreply.github.com>
2020-08-28 10:38:38 +1000

    Update scripts

--- repos/scripts/similarityscript.js
+++ repos/scripts/similarityscript.js
@@ -19,23 +19,21 @@ function getMarkdown(ms, target)
 
 function refreshMarkdown_Shell()
 {
-  document.getElementsById("shell").setAttribute("file", "https://ceresbakalite.github.io/similarity/shell/README.md?" + getRandomInteger(10000,1000000));
+  document.getElementsById("shell")[0].setAttribute("file", "https://ceresbakalite.github.io/similarity/shell/README.md?" + getRandomInteger(10000,1000000));
 }
 
 function refreshMarkdown_Repos()
 {
   alert('1');
-  document.getElementById("repos").setAttribute("file", "https://ceresbakalite.github.io/similarity/README.md?" + getRandomInteger(10000,1000000));
+  document.getElementsById("repos")[0].setAttribute("file", "https://ceresbakalite.github.io/similarity/README.md?" + getRandomInteger(10000,1000000));
   alert('2');
-  document.getElementById("logo").setAttribute("src", "https://ceresbakalite.github.io/similarity/images/NAVSimilarityLogoRepos.png");
+  document.getElementsById("logo")[0].setAttribute("src", "https://ceresbakalite.github.io/similarity/images/NAVSimilarityLogoRepos.png");
   alert('3');
 }
 
 function waitForMarkdown()
 {
-  document.getElementById("shell").style.display = "block";
-  document.getElementById("repos").style.display = "block";
-  document.getElementById("site-footer-display").style.display = "block";
+  document.getElementsById("site-footer-display")[0].style.display = "block";
 }
 
 function getRandomInteger(min, max)
