bc35f1ece5b8fc56fb4c8c8f907589bddc9d8902
ceresbakalite <ceres@users.noreply.github.com>
2020-08-29 07:09:52 +1000

    Update similarityscript.js

--- repos/scripts/similarityscript.js
+++ repos/scripts/similarityscript.js
@@ -23,22 +23,28 @@ function getMarkdown(ms, target)
 
 function refreshMarkdown_Index()
 {
-  document.getElementsByTagName("zero-md")[0].setAttribute("file", "https://ceresbakalite.github.io/similarity/repos/scripts/index.md?" + getRandomInteger(10000,1000000));
+  WebComponents.waitFor(() =>
+  {
+    let el = document.getElementsByTagName('zero-md')[0];
+    el.setAttribute('src') = 'https://ceresbakalite.github.io/similarity/repos/scripts/index.md?' + getRandomInteger(10000,1000000);
+  });
+
+  //document.getElementsByTagName('zero-md')[0].setAttribute('file', 'https://ceresbakalite.github.io/similarity/repos/scripts/index.md?' + getRandomInteger(10000,1000000));
 }
 
 function refreshMarkdown_Shell()
 {
-  document.getElementsByTagName("zero-md")[0].setAttribute("file", "https://ceresbakalite.github.io/similarity/shell/README.md?" + getRandomInteger(10000,1000000));
+  document.getElementsByTagName('zero-md')[0].setAttribute('file', 'https://ceresbakalite.github.io/similarity/shell/README.md?' + getRandomInteger(10000,1000000));
 }
 
 function refreshMarkdown_Repos()
 {
-  document.getElementsByTagName("zero-md")[0].setAttribute("file", "https://ceresbakalite.github.io/similarity/README.md?" + getRandomInteger(10000,1000000));
+  document.getElementsByTagName('zero-md')[0].setAttribute('file', 'https://ceresbakalite.github.io/similarity/README.md?' + getRandomInteger(10000,1000000));
 }
 
 function waitForMarkdown()
 {
-  document.getElementById("site-footer-display").style.display = "block";
+  document.getElementById('site-footer-display').style.display = 'block';
 }
 
 function getRandomInteger(min, max)
