44a5eb34a71c1ccecf980843be266b919f1c9d06
ceresbakalite <ceres@users.noreply.github.com>
2020-08-29 03:47:22 +1000

    Update similarityscript.js

--- repos/scripts/similarityscript.js
+++ repos/scripts/similarityscript.js
@@ -4,12 +4,13 @@ function getMarkdown(ms, target)
   {
     case 'shell':
       refreshMarkdown_Shell();
+      setInterval(waitForMarkdown(null, false), ms);
       break;
 
     case 'repos':
       alert('repose');
       refreshMarkdown_Repos();
-      resetMarkdown_Logo("https://ceresbakalite.github.io/similarity/images/NAVSimilarityLogoRepos.png");
+      setInterval(waitForMarkdown("https://ceresbakalite.github.io/similarity/images/NAVSimilarityLogoRepos.png", true), ms);
       alert('end repose');
       break;
 
@@ -17,7 +18,6 @@ function getMarkdown(ms, target)
       refreshMarkdown_Shell();
   }
 
-  var interval = setInterval(waitForMarkdown, ms);
 }
 
 function refreshMarkdown_Shell()
@@ -30,30 +30,9 @@ function refreshMarkdown_Repos()
   document.getElementsByTagName("zero-md")[0].setAttribute("file", "https://ceresbakalite.github.io/similarity/README.md?" + getRandomInteger(10000,1000000));
 }
 
-function resetMarkdown_Logo(url)
+function waitForMarkdown(url = null, resetlogo = false)
 {
-  url = (url == null) ? "https://ceresbakalite.github.io/similarity/images/NAVSimilarityLogoRepos.png" : url;
-
-  alert('try 1: ' + url);
-
-  document.getElementsByTagName("img")[0].setAttribute("src", url);
-
-  alert('try 2: ' + url);
-
-  WebComponents.waitFor(() =>
-  {
-    var el = document.createElement("img");
-    el.setAttribute("src", url);
-    el.setAttribute("align", "right");
-    document.getElementById("logo-container").appendChild("el");
-  });
-
-  //document.getElementById("logo-default").setAttribute("src", "https://ceresbakalite.github.io/similarity/images/NAVSimilarityLogoRepos.png");
-  alert('src: ' + el.src);
-}
-
-function waitForMarkdown()
-{
+  if (resetlogo) resetMarkdown_Logo(url);
   document.getElementById("site-footer-display").style.display = "block";
 }
 
