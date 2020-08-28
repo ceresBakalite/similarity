862a6604ed7dfe8ffc703e817b505e1d3bf86d4e
ceresbakalite <ceres@users.noreply.github.com>
2020-08-29 02:17:18 +1000

    Update similarityscript.js

--- repos/scripts/similarityscript.js
+++ repos/scripts/similarityscript.js
@@ -10,6 +10,7 @@ function getMarkdown(ms, target)
       alert('repose');
       refreshMarkdown_Repos();
       resetMarkdown_Logo("https://ceresbakalite.github.io/similarity/images/NAVSimilarityLogoRepos.png");
+      document.getElementById("logo-default").setAttribute("src", "https://ceresbakalite.github.io/similarity/images/NAVSimilarityLogoRepos.png");
       alert('end repose');
       break;
 
