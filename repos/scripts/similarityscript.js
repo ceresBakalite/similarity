dac3294dd918392eaba31caa14de61a443379d03
ceresbakalite <ceres@users.noreply.github.com>
2020-08-28 10:20:24 +1000

    Update scripts

--- repos/scripts/similarityscript.js
+++ repos/scripts/similarityscript.js
@@ -19,18 +19,18 @@ function getMarkdown(ms, target)
 
 function refreshMarkdown_Shell()
 {
-  document.getElementsByTagName("zero-md").setAttribute("file", "https://ceresbakalite.github.io/similarity/shell/README.md?" + getRandomInteger(10000,1000000));
+  document.getElementsByTagName("zero-md")[0].setAttribute("file", "https://ceresbakalite.github.io/similarity/shell/README.md?" + getRandomInteger(10000,1000000));
 }
 
 function refreshMarkdown_Repos()
 {
-  document.getElementByTagName("zero-md").setAttribute("file", "https://ceresbakalite.github.io/similarity/README.md?" + getRandomInteger(10000,1000000));
+  document.getElementByTagName("zero-md")[0].setAttribute("file", "https://ceresbakalite.github.io/similarity/README.md?" + getRandomInteger(10000,1000000));
   document.getElementByTagName("img")[0].setAttribute("src", "https://ceresbakalite.github.io/similarity/images/NAVSimilarityLogoRepos.png");
 }
 
 function waitForMarkdown()
 {
-  document.getElementByTagName("zero-md").style.display = "block";
+  document.getElementByTagName("zero-md")[0].style.display = "block";
   document.getElementById("site-footer-display").style.display = "block";
 }
 
