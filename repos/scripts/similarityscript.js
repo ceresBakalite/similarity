4a0d54cf80d87a6ba3502e7a64144e2731e9c568
ceresbakalite <ceres@users.noreply.github.com>
2020-08-31 04:54:23 +1000

    Release version 1.0b

--- repos/scripts/similarityscript.js
+++ repos/scripts/similarityscript.js
@@ -8,51 +8,117 @@ customElements.define('include-directive', class extends HTMLElement
 
 });
 
-function getMarkdown(ms, target)
+function addScrollEventListener()
 {
-  switch (target)
+  return;
+  alert('1');
+  let iframe = document.getElementsByTagName('frame-container')[0];
+  alert('2');
+
+if (iframe == null) alert('its null');
+
+  iframe.contentDocument.body.innerHTML = 'a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>';
+  alert('3');
+
+  iframe.contentDocument.addEventListener('scroll', function(event) {
+    console.log(event);
+  }, false);
+  alert('4');
+
+}
+
+
+function onloadComplete(ms, md)
+{
+  switch (md)
   {
     case 'index':
-      refreshMarkdown('index-md', 'https://ceresbakalite.github.io/similarity/repos/scripts/index.md?' + getRandomInteger(10000,1000000));
+      setInterval(waitForMarkdown('index-md'), ms);
       break;
 
     case 'shell':
-      refreshMarkdown('shell-md', 'https://ceresbakalite.github.io/similarity/shell/README.md?' + getRandomInteger(10000,1000000));
+      setInterval(waitForMarkdown('shell-md'), ms);
       break;
 
     case 'repos':
-      refreshMarkdown('repos-md', 'https://ceresbakalite.github.io/similarity/README.md?' + getRandomInteger(10000,1000000));
+      setInterval(waitForMarkdown('repos-md'), ms);
       break;
 
     default:
-      refreshMarkdown('index-md', 'https://ceresbakalite.github.io/similarity/repos/scripts/index.md?' + getRandomInteger(10000,1000000));
+      setInterval(waitForMarkdown('index-md'), ms);
       break;
 
   }
 
-  setInterval(waitForMarkdown, ms);
 }
 
-function refreshMarkdown(target, location)
+function selectMarkdownDocument(md)
 {
-  WebComponents.waitFor(() =>
+  switch (md)
   {
-      let el = document.getElementById(target);
-      el.setAttribute('src', location);
-  });
+    case 'index':
+      getMarkdownDocument('index', 'https://ceresbakalite.github.io/similarity/repos/scripts/SyncIndex.html');
+      break;
+
+    case 'shell':
+      getMarkdownDocument('shell', 'https://ceresbakalite.github.io/similarity/repos/scripts/SyncShell.html');
+      break;
+
+    case 'repos':
+      getMarkdownDocument('repos', 'https://ceresbakalite.github.io/similarity/repos/scripts/SyncRepos.html');
+      break;
+
+    default:
+      getMarkdownDocument('index', 'test01.html');
+      break;
+
+  }
 
 }
 
-function waitForMarkdown()
+function getMarkdownDocument(id, target)
 {
-  WebComponents.waitFor(() =>
-  {
+    document.getElementById('frame-container').setAttribute('src', target);
+    document.getElementById(id).blur();
+}
+
+function waitForMarkdown(target)
+{
+  WebComponents.waitFor(() =>  {
+
+    if (refreshMarkdown(target))
+    {
+      document.getElementById('footer-content').style.display = 'block';
       document.getElementById('site-footer-display').style.display = 'block';
+    }
+
   });
 
+}
+
+function refreshMarkdown(target)
+{
+  if (document.getElementById(target))
+  {
+    document.getElementById(target).setAttribute('src', document.getElementById(target).getAttribute('src') + '?' + getRandomInteger(10000,1000000));
+
+  } else if (document.getElementsByTagName('zero-md')[0]) {
+    document.getElementsByTagName('zero-md')[0].setAttribute('src', document.getElementsByTagName('zero-md')[0].getAttribute('src') + '?' + getRandomInteger(10000,1000000));
+
+  } else {
+    return false;
+
+  }
+
+  return true;
 }
 
 function getRandomInteger(min, max)
 {
   return Math.floor(Math.random() * (max - min) ) + min;
+}
+
+function adjustHeaderDisplay(attribute)
+{
+  document.getElementById('site-header-display').style.display = attribute;
 }
