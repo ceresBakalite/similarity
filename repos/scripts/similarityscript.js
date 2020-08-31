ac5de6ad3b1803a048460a8c7c4b459ace980533
ceresbakalite <ceres@users.noreply.github.com>
2020-08-31 22:29:20 +1000

    Transitions

--- repos/scripts/similarityscript.js
+++ repos/scripts/similarityscript.js
@@ -28,14 +28,19 @@ function scrollEventListener()
 
 function adjustHeaderDisplay()
 {
+  var hint = parent.document.getElementById('site-header-display');
+  
   if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300)
   {
+    if (hint.classList.contains('h-hide')) hint.classList.remove('h-hide');
 
-    parent.document.getElementById('site-header-display').style.display = 'none';
+    //parent.document.getElementById('site-header-display').style.display = 'none';
 
   } else {
 
-    parent.document.getElementById('site-header-display').style.display = 'block';
+    if (hint.classList.contains('h-hide')) hint.classList.add('h-hide');
+
+//    parent.document.getElementById('site-header-display').style.display = 'block';
 
   }
 
