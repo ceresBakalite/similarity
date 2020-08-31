75a0bbcc48f91867902fc1c342653cfb7d273e7c
ceresbakalite <ceres@users.noreply.github.com>
2020-08-31 20:09:13 +1000

    Update similarityscript.js

--- repos/scripts/similarityscript.js
+++ repos/scripts/similarityscript.js
@@ -30,8 +30,9 @@ function adjustHeaderDisplay()
 {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50)
   {
-    //if (parent.document.getElementsByTagName('div')[0] == 'block') alert('say argh hello');
+    if (parent.document.getElementById('site-header-display')) alert('yay');
     //if (parent.document.getElementById('site-header-display') == 'block') alert('say hello');
+    //if (parent.document.getElementsByTagName('div')[0] == 'block') alert('say argh hello');
     //alert('more argh');
     // document.getElementById('site-header-display').className = 'site-header-slide';
 
