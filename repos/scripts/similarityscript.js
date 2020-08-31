9dd107e958e40f69b2a4ba4b167283ff7a3e49c8
ceresbakalite <ceres@users.noreply.github.com>
2020-09-01 09:27:45 +1000

    Update similarityscript.js

--- repos/scripts/similarityscript.js
+++ repos/scripts/similarityscript.js
@@ -32,13 +32,13 @@ function adjustHeaderDisplay()
 
   if (document.body.scrollTop > 25 || document.documentElement.scrollTop > 25)
   {
-    el.style.visibility = 'hidden';
-    //el.style.display = 'none';
+    //el.style.visibility = 'hidden';
+    el.style.display = 'none';
 
   } else {
 
-    el.style.visibility = 'visible';
-    //el.style.display = 'block';
+    //el.style.visibility = 'visible';
+    el.style.display = 'block';
 
   }
 
