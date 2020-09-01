e1875525b9485671f262252d6a6db9970003dd49
ceresbakalite <ceres@users.noreply.github.com>
2020-09-01 10:47:11 +1000

    Update similarityscript.js

--- repos/scripts/similarityscript.js
+++ repos/scripts/similarityscript.js
@@ -26,6 +26,25 @@ function scrollEventListener()
   window.onscroll = function(){ adjustHeaderDisplay(); };
 }
 
+const animateCSS = (element, animation, prefix = 'animate__') =>
+  // We create a Promise and return it
+  new Promise((resolve, reject) => {
+    const animationName = `${prefix}${animation}`;
+    const node = document.querySelector(element);
+
+    node.classList.add(`${prefix}animated`, animationName);
+
+    // When the animation ends, we clean the classes and resolve the Promise
+    function handleAnimationEnd() {
+      node.classList.remove(`${prefix}animated`, animationName);
+      node.removeEventListener('animationend', handleAnimationEnd);
+
+      resolve(element.style.display = 'block');
+    }
+
+    node.addEventListener('animationend', handleAnimationEnd);
+  });
+
 function adjustHeaderDisplay()
 {
   let el = parent.document.getElementById('site-header-display');
@@ -34,13 +53,14 @@ function adjustHeaderDisplay()
   {
     el.classList.add('animate__animated', 'animate__fadeOutUp');
 
-    el.addEventListener('animationend', () => {
-      el.style.display = 'none';
+    animateCSS(el, 'fadeOutUp').then((message) => {
+      // Do something after the animation
     });
-  } else {
+
+    } else {
 
     el.classList.add('animate__animated', 'animate__fadeInDown');
-    el.style.display = 'block';
+
 
     el.removeEventListener('animationend', ());
     el.classList.remove('animate__animated', 'animate__fadeInDown');
