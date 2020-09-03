55ac4786e011eab8bb55579eee376bec71bc37a0
ceresbakalite <ceres@users.noreply.github.com>
2020-09-03 18:56:08 +1000

    Update similarityscriptslide.js

--- repos/scripts/similarityscriptslide.js
+++ repos/scripts/similarityscriptslide.js
@@ -42,7 +42,16 @@ function showSlidesRepeat()
   setTimeout(showSlidesRepeat, 2000); // Change image every 2 seconds
 }
 
-function relocateImage(id)
+function //relocateImage(id)
 {
     window.open(location.href = document.getElementById(id).getAttribute('src'), 'Image');
+}
+
+function relocateImage(id) {
+   var largeImage = document.getElementById(id);
+   largeImage.style.display = 'block';
+   largeImage.style.width=200+"px";
+   largeImage.style.height=200+"px";
+   var url=largeImage.getAttribute('src');
+   window.open(url,'Image','width=largeImage.stylewidth,height=largeImage.style.height,resizable=1');
 }
