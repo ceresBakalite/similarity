4618021f234aa42d18be6ecfac73acee4e82baaf
ceresbakalite <ceres@users.noreply.github.com>
2020-09-03 19:22:00 +1000

    Update scripts

--- repos/scripts/similarityscriptslide.js
+++ repos/scripts/similarityscriptslide.js
@@ -3,47 +3,46 @@ var viewIndex = 1;
 // Next/previous controls
 function getSlide(n)
 {
-  showSlidesNoRepeat(viewIndex += n);
+    showSlidesNoRepeat(viewIndex += n);
 }
 
 // Thumbnail image controls
 function currentSlide(n)
 {
-  showSlidesNoRepeat(viewIndex = n);
+    showSlidesNoRepeat(viewIndex = n);
 }
 
 function showSlidesNoRepeat(n = 1)
 {
-  let slides = document.getElementsByClassName('slideview');
-  let dots = document.getElementsByClassName('dot');
+    let slides = document.getElementsByClassName('slideview');
+    let dots = document.getElementsByClassName('dot');
 
-  if (n > slides.length) viewIndex = 1;
+    if (n > slides.length) viewIndex = 1;
 
-  if (n < 1) viewIndex = slides.length;
+    if (n < 1) viewIndex = slides.length;
 
-  for (var i = 0; i < slides.length; i++) { slides[i].style.display = 'none'; }
-  for (var i = 0; i < dots.length; i++) { dots[i].className = dots[i].className.replace(' active', ''); }
+    for (let i = 0; i < slides.length; i++) { slides[i].style.display = 'none'; }
+    for (let i = 0; i < dots.length; i++) { dots[i].className = dots[i].className.replace(' active', ''); }
 
-  slides[viewIndex-1].style.display = 'block';
-  dots[viewIndex-1].className += ' active';
+    slides[viewIndex-1].style.display = 'block';
+    dots[viewIndex-1].className += ' active';
 }
 
 function showSlidesRepeat()
 {
-  let slides = document.getElementsByClassName('slideview');
+    let slides = document.getElementsByClassName('slideview');
 
-  for (var i = 0; i < slides.length; i++) { slides[i].style.display = 'none'; }
+    for (let i = 0; i < slides.length; i++) { slides[i].style.display = 'none'; }
 
-  viewIndex++;
+    viewIndex++;
 
-  if (viewIndex > slides.length) viewIndex = 1;
-  slides[viewIndex-1].style.display = 'block';
+    if (viewIndex > slides.length) viewIndex = 1;
+    slides[viewIndex-1].style.display = 'block';
 
-  setTimeout(showSlidesRepeat, 2000); // Change image every 2 seconds
+    setTimeout(showSlidesRepeat, 2000); // Change image every 2 seconds
 }
 
 function relocateImage(el)
 {
-  alert(el.getAttribute('src'));
     window.open(el.getAttribute('src'), 'Image');
 }
