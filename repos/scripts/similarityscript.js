5867c9df190a14175846514e43c62552d8f9f479
ceresbakalite <ceres@users.noreply.github.com>
2020-09-04 09:31:36 +1000

    Update similarityscript.js

--- repos/scripts/similarityscript.js
+++ repos/scripts/similarityscript.js
@@ -135,7 +135,7 @@ function adjustHeaderDisplay()
 
     if (pin == 'disabled')
     {
-        alert(window.innerHeight + ' - ' + window.outerHeight + ' - ' + parent.window.innerHeight + ' - ' + parent.window.outerHeight + ' - ' + window.scrollY);
+        alert((window.outerHeight - window.innerHeight) + ' - ' + trigger);
 
         var el = parent.document.getElementById('site-header-display');
 
