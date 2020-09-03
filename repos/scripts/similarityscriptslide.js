e7e875a797f297a036f2a3ddfd4c4a74d30a8218
ceresbakalite <ceres@users.noreply.github.com>
2020-09-03 15:21:43 +1000

    Update similarityscriptslide.js

--- repos/scripts/similarityscriptslide.js
+++ repos/scripts/similarityscriptslide.js
@@ -10,49 +10,33 @@ function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
-function showSlidesNoRepeat(n = 1) {
-  alert('1');
-
+function showSlidesNoRepeat(n = 1)
+{
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
 
-  alert('2');
+  if (n > slides.length) slideIndex = 1;
 
-  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) slideIndex = slides.length;
 
-  if (n < 1) {slideIndex = slides.length}
-
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-
-  alert('3');
-
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-
-  alert('4');
+  for (i = 0; i < slides.length; i++) { slides[i].style.display = "none"; }
+  for (i = 0; i < dots.length; i++) { dots[i].className = dots[i].className.replace(" active", ""); }
 
   slides[slideIndex-1].style.display = "block";
-
-  alert('5');
-
   dots[slideIndex-1].className += " active";
-
-  alert('6');
-
 }
 
 function showSlidesRepeat() {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
+
+  for (i = 0; i < slides.length; i++) { slides[i].style.display = "none"; }
+
   slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
+
+  if (slideIndex > slides.length) slideIndex = 1;
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+
+  setTimeout(showSlidesRepeat, 2000); // Change image every 2 seconds
 }
