8ae0be9cabcd6324749ec85afe2fd3e20c1b89e8
ceresbakalite <ceres@users.noreply.github.com>
2020-09-03 23:19:35 +1000

    Update similarityscriptslide.js

--- repos/scripts/similarityscriptslide.js
+++ repos/scripts/similarityscriptslide.js
@@ -40,13 +40,17 @@ function startSlideViewerRepeat()
     slideRepeatTrigger = true;
 
     let slides = document.getElementsByClassName('slideview');
+    let dots = document.getElementsByClassName('dot');
 
     for (let i = 0; i < slides.length; i++) { slides[i].style.display = 'none'; }
+    for (let i = 0; i < dots.length; i++) { dots[i].className = dots[i].className.replace(' active', ''); }
 
     slideIndex++;
 
     if (slideIndex > slides.length) slideIndex = 1;
+
     slides[slideIndex-1].style.display = 'block';
+    dots[slideIndex-1].className += ' active';
 
     slideRepeat = setTimeout(startSlideViewerRepeat, slideInterval);
 }
