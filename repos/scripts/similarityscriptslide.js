dd0929e889a3f5dfff1713214f012b1d5699cbcc
ceresbakalite <ceres@users.noreply.github.com>
2020-09-04 16:21:07 +1000

    Update similarityscriptslide.js

--- repos/scripts/similarityscriptslide.js
+++ repos/scripts/similarityscriptslide.js
@@ -1,24 +1,43 @@
-var ceresview = {}
 
-ceresview.slideInterval = 5000;
-ceresview.slideRepeatInterval = 25000;
+var ceresview = (function()
+{
+    var index = 1;
 
-ceresview.slideIndex = 1;
-ceresview.slideLastSlideTime = ceresview.getTimeNow();
+    return
+    {
+        next: function()
+        {
+            return index++;
+        },
 
-ceresview.getTimeNow = function()
+        reset: function()
+        {
+            index = 0;
+        }
+
+    };
+
+})();
+
+slideInterval = 5000;
+slideRepeatInterval = 25000;
+
+slideIndex = 1;
+slideLastSlideTime = getTimeNow();
+
+var getTimeNow = function()
 {
     return new Date().getTime();
 };
 
 function getSlide(n)
 {
-    startSlideViewer(ceresview.slideIndex += n);
+    startSlideViewer(slideIndex += n);
 };
 
 function setSlide(n)
 {
-    startSlideViewer(ceresview.slideIndex = n);
+    startSlideViewer(slideIndex = n);
 };
 
 function startSlideViewer(n)
@@ -26,13 +45,13 @@ function startSlideViewer(n)
     let slides = document.getElementsByClassName('slideview');
     let dots = document.getElementsByClassName('dot');
 
-    ceresview.slideIndex = (n < 1) ? slides.length : (n > slides.length) ? 1 : ceresview.slideIndex;
+    slideIndex = (n < 1) ? slides.length : (n > slides.length) ? 1 : slideIndex;
 
     for (let i = 0; i < slides.length; i++) { slides[i].style.display = 'none'; }
     for (let i = 0; i < dots.length; i++) { dots[i].className = dots[i].className.replace(' active', ''); }
 
-    slides[ceresview.slideIndex-1].style.display = 'block';
-    dots[ceresview.slideIndex-1].className += ' active';
+    slides[slideIndex-1].style.display = 'block';
+    dots[slideIndex-1].className += ' active';
 };
 
 function openImageTab(el)
