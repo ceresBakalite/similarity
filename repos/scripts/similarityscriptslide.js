3c7f477328d4ef214a531c3c42661a3d5f280b8f
ceresbakalite <ceres@users.noreply.github.com>
2020-09-04 20:47:17 +1000

    Update slide namespace

--- repos/scripts/similarityscriptslide.js
+++ repos/scripts/similarityscriptslide.js
@@ -1,56 +1,24 @@
-//var slideview = {}
-//slideview.index = 1;
-
-//slideview.openImageTab = function(el)
-//{
-//    window.open(el.getAttribute('src'), 'image');
-//}
-
-//slideview.getSlide = function(i)
-//{
-//    slideview.startSlideViewer(slideview.index += i);
-//}
-
-//slideview.setSlide = function(i)
-//{
-//    slideview.startSlideViewer(slideview.index = i);
-//}
-
-//slideview.startSlideViewer = function(n)
-//{
-//   let slides = document.getElementsByClassName('slideview');
-//    let dots = document.getElementsByClassName('dot');
-
-//    slideview.index = (n < 1) ? slides.length : (n > slides.length) ? 1 : slideview.index;
-
-//    for (let i = 0; i < slides.length; i++) { slides[i].style.display = 'none'; }
-//    for (let i = 0; i < dots.length; i++) { dots[i].className = dots[i].className.replace(' active', ''); }
-
-//    slides[slideview.index-1].style.display = 'block';
-//    dots[slideview.index-1].className += ' active';
-//}
-
-var slideview = {};
-(function(context)
+var ceres = {};
+(function(slideview)
 {
     var index = 1;
 
-    context.openImageTab = function(el)
+    slideview.openImageTab = function(el)
     {
         window.open(el.getAttribute('src'), 'image');
     };
 
-    context.getSlide = function(i)
+    slideview.getSlide = function(i)
     {
-        context.startSlideViewer(index += i);
+        slideview.startSlideViewer(index += i);
     };
 
-    context.setSlide = function(i)
+    slideview.setSlide = function(i)
     {
-        context.startSlideViewer(index = i);
+        slideview.startSlideViewer(index = i);
     };
 
-    context.startSlideViewer = function(n)
+    slideview.startSlideViewer = function(n)
     {
         let slides = document.getElementsByClassName('slideview');
         let dots = document.getElementsByClassName('dot');
@@ -63,4 +31,4 @@ var slideview = {};
         slides[index-1].style.display = 'block';
         dots[index-1].className += ' active';
     };
-})(slideview);
+})(ceres);
