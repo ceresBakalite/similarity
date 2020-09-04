94566f6c462bfe69fe4ef6f951495cd8091c5c1f
ceresbakalite <ceres@users.noreply.github.com>
2020-09-04 18:08:51 +1000

    Update similarityscriptslide.js

--- repos/scripts/similarityscriptslide.js
+++ repos/scripts/similarityscriptslide.js
@@ -1,14 +1,14 @@
-var ceresbakalite = {}
-ceresbakalite.index = 1;
+var slideview = {}
+slideview.index = 1;
 
 function getSlide(index)
 {
-    startSlideViewer(ceresbakalite.index += index);
+    startSlideViewer(slideview.index += index);
 };
 
 function setSlide(index)
 {
-    startSlideViewer(ceresbakalite.index = index);
+    startSlideViewer(slideview.index = index);
 };
 
 function startSlideViewer(n)
@@ -16,13 +16,13 @@ function startSlideViewer(n)
     let slides = document.getElementsByClassName('slideview');
     let dots = document.getElementsByClassName('dot');
 
-    ceresbakalite.index = (n < 1) ? slides.length : (n > slides.length) ? 1 : ceresbakalite.index;
+    slideview.index = (n < 1) ? slides.length : (n > slides.length) ? 1 : slideview.index;
 
     for (let i = 0; i < slides.length; i++) { slides[i].style.display = 'none'; }
     for (let i = 0; i < dots.length; i++) { dots[i].className = dots[i].className.replace(' active', ''); }
 
-    slides[ceresbakalite.index-1].style.display = 'block';
-    dots[ceresbakalite.index-1].className += ' active';
+    slides[slideview.index-1].style.display = 'block';
+    dots[slideview.index-1].className += ' active';
 };
 
 function openImageTab(el)
