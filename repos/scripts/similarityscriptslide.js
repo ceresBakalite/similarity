62718d223766205e794ebc3c3e53923f1cb7fac1
ceresbakalite <ceres@users.noreply.github.com>
2020-09-09 18:34:22 +1000

    Update similarityscriptslide.js

--- repos/scripts/similarityscriptslide.js
+++ repos/scripts/similarityscriptslide.js
@@ -164,7 +164,7 @@ var ceres = {};
 
             displaySlide();
 
-            setTimeout(function(){ activateContainer(); }, 500);
+            setTimeout(function(){ createAttribute(slideview.container + '-image-container', 'style', 'display: block;'); }, 500);
 
             function createSlideviewPointerContainer()
             {
@@ -178,7 +178,6 @@ var ceres = {};
                 progenitor.appendChild(pointerElement);
 
                 createAttribute(pointerElement.id, 'class', 'slideview-pointer-container');
-                createAttribute(pointerElement.id, 'style', 'display: none;');
 
                 for (let item = 0; item < attributes.length; item++)
                 {
@@ -274,12 +273,6 @@ var ceres = {};
                 createAttribute(el.id, 'src', getURL());
 
                 if (markup) document.getElementById(el.id).innerHTML = markup;
-            }
-
-            function activateContainer()
-            {
-                createAttribute(slideview.container + '-image-container', 'style', 'display: block;');
-                createAttribute(slideview.container + '-pointer-container', 'style', 'display: block;');
             }
 
         }
