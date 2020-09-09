7455add4d4ac368c93ab897d318711521c35df3c
ceresbakalite <ceres@users.noreply.github.com>
2020-09-09 10:48:38 +1000

    Update similarityscriptslide.js

--- repos/scripts/similarityscriptslide.js
+++ repos/scripts/similarityscriptslide.js
@@ -86,7 +86,7 @@ var ceres = {};
                     {
                         list = (progenitor.innerHTML) ? progenitor.innerHTML : null;
                         if (list || retryAttempt == retryLimit) clearInterval(interval);
-                        i++;
+                        retryAttempt++;
                     }
 
                     return list;
