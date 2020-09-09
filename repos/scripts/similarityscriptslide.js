a176f28fcb1c894d3fca208b34c2eb130ca89a29
ceresbakalite <ceres@users.noreply.github.com>
2020-09-09 10:21:48 +1000

    Update similarityscriptslide.js

--- repos/scripts/similarityscriptslide.js
+++ repos/scripts/similarityscriptslide.js
@@ -70,8 +70,45 @@ var ceres = {};
 
             function awaitImageList()
             {
-                return (progenitor.innerHTML) ? progenitor.innerHTML : getEmbedImageList();
+                if (progenitor.innerHTML) return progenitor.innerHTML;
+
+                let list = getEmbedImageList();
+                if (!list) setTimeout(function(){ lookAgain(); }, 1000);
+
+                return list;
+
+                function lookAgain()
+                {
+                    list = (progenitor.innerHTML) ? progenitor.innerHTML : null;
+                }
+
             }
+
+
+            //1. Create a new function that returns a promise
+            //    function firstFunction() {
+            //      return new Promise((resolve, reject) => {
+            //          let y = 0
+            //          setTimeout(() => {
+            //            for(i=0; i<10; i++){
+            //               y++
+            //            }
+            //             console.log('loop completed')
+            //             resolve(y)
+            //          }, 2000)
+            //      })
+            //    }
+
+            //2. Create an async function
+            //    async function secondFunction() {
+            //        console.log('before promise call')
+            //        //3. Await for the first function to complete
+            //        let result = await firstFunction()
+            //        console.log('promise resolved: ' + result)
+            //        console.log('next step')
+            //    };
+
+            //    secondFunction()
 
         }
 
