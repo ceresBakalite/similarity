7b1debaffc3b3aaebf236cdbb686d4dc4c3593dd
ceresbakalite <ceres@users.noreply.github.com>
2020-09-16 11:40:30 +1000

    Update similarityscriptslide.js

--- repos/scripts/similarityscriptslide.js
+++ repos/scripts/similarityscriptslide.js
@@ -108,7 +108,7 @@ let ceres = {};
                     function getMarkupList()
                     {
                         //const el = (csv.progenitor.innerHTML) ? csv.progenitor.innerHTML : null; document.getElementById('ceres-csv') ? document.getElementById('ceres-csv') : document.getElementsByTagName('ceres-csv')[0];
-                        const el = csv.progenitor.getElementById('ceres-csv') ? csv.progenitor.getElementById('ceres-csv') : csv.progenitor.getElementsByTagName('ceres-csv')[0];
+                        const el = document.getElementById('ceres-csv') ? document.getElementById('ceres-csv') : document.getElementsByTagName('ceres-csv')[0];
                         const list = (el) ? el.innerHTML : null;
 
 alert('list: ' + list);
