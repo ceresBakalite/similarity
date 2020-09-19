8bdb54adc92897f0d2bf4ee24737fe5344960a75
ceresbakalite <ceres@users.noreply.github.com>
2020-09-20 02:16:00 +1000

    Update similarityscript.js

--- repos/scripts/similarityscript.js
+++ repos/scripts/similarityscript.js
@@ -31,8 +31,8 @@ let similarity = {};
 
     function getMarkupDocument(mu)
     {
-        console.log('typeset.markup: ' typeset.markup + ' - mu: ' + mu);
-        
+        console.log('typeset.markup: ' + typeset.markup + ' - mu: ' + mu);
+
         if (document.getElementById(mu)) typeset.markup = (typeset.markup != mu) ? mu : typeset.markup;
 
         document.getElementById('frame-container').setAttribute('src', getMarkupLocation());
