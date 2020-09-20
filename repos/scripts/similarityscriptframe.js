3e7418a7828b5195b01e08d58e01bc400c24fb7e
ceresbakalite <ceres@users.noreply.github.com>
2020-09-20 18:35:32 +1000

    Split javascript files

--- repos/scripts/similarityscriptframe.js
+++ repos/scripts/similarityscriptframe.js
@@ -1,4 +1,4 @@
-let similarity = {};
+let frame = {};
 (function(ceres)
 {
     window.customElements.define('include-directive', class extends HTMLElement
@@ -11,78 +11,7 @@ let similarity = {};
 
     });
 
-    ceres.onloadPrimary = function() { onloadPrimary(); }; // public method reference
-    ceres.xxxxonloadFrame = function(id) { onloadFrame(id); };  // public method reference
-    ceres.getMarkupDocument = function(id, el) { getMarkupDocument(id, el); };  // public method reference
-    ceres.resetPinState = function(el) { resetPinState(el); };  // public method reference
-
-    class component
-    {
-        constructor()
-        {
-            this.attribute = function() { return attribute; }
-        }
-
-    }
-
-    let resource = new component();
-    let location = new Map()
-
-    setResourcePrecursors();
-
-    function getQueryString()
-    {
-        const urlParams = new URLSearchParams(window.location.search);
-        const markupId = urlParams.get('mu')
-
-        if (markupId) getMarkupDocument(markupId);
-    }
-
-    function getMarkupDocument(markupId, buttonElement)
-    {
-        if (resource.attribute.markupId != markupId)
-        {
-            resource.attribute.markupId = markupId;
-            resource.attribute.markupUrl = (location.has(resource.attribute.markupId)) ? location.get(resource.attribute.markupId) : location.get('index');
-
-            document.getElementById('frame-container').setAttribute('src', resource.attribute.markupUrl);
-        }
-
-        if (buttonElement) buttonElement.blur();
-    }
-
-    function onloadPrimary()
-    {
-        getQueryString();
-    }
-
-    function setResourcePrecursors()
-    {
-        location.set('index', 'https://ceresbakalite.github.io/similarity/repos/scripts/SyncIndex.html');
-        location.set('shell', 'https://ceresbakalite.github.io/similarity/repos/scripts/SyncShell.html');
-        location.set('slide', 'https://ceresbakalite.github.io/similarity/repos/scripts/SyncSlide.html');
-        location.set('repos', 'https://ceresbakalite.github.io/similarity/repos/scripts/SyncRepos.html');
-
-        resource.attribute.markupId = 'index';
-        resource.attribute.markupUrl = location.get(resource.attribute.markupId);
-    }
-
-    function resetPinState(el)
-    {
-        if (el.getAttribute('state') == 'enabled')
-        {
-            el.src = "https://ceresbakalite.github.io/similarity/images/NAVPinIconDisabled.png";
-            el.setAttribute('state', 'disabled');
-            adjustHeaderDisplay();
-
-        } else {
-
-            el.src = "https://ceresbakalite.github.io/similarity/images/NAVPinIconEnabled.png";
-            el.setAttribute('state', 'enabled');
-
-        }
-
-    }
+    ceres.onloadFrame = function(id) { onloadFrame(id); };  // public method reference
 
     function onloadFrame(markupId)
     {
@@ -194,4 +123,4 @@ let similarity = {};
         return null;
     }
 
-})(similarity);
+})(frame);
