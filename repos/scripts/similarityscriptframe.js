576605857115dd2d28803cc5cf996cdafcdd1c34
ceresbakalite <ceres@users.noreply.github.com>
2020-09-28 02:05:56 +1000

    Update similarityscriptframe.js

--- repos/scripts/similarityscriptframe.js
+++ repos/scripts/similarityscriptframe.js
@@ -113,39 +113,39 @@ let similarityframe = {};
 
     }
 
-    function setCookie(cn, cv, ex = 0)
-    {
-        if (cn && cv)
-        {
-            let dt = new Date();
-            dt.setTime(dt.getTime() + (ex * 24 * 60 * 60 * 1000));
-            let expires = "expires=" + dt.toUTCString();
-
-            document.cookie = cn + "=" + cv + ";" + expires + ";path=/";
-        }
-
-    }
-
-    function getCookie(cn)
-    {
-        if (cn)
-        {
-            let cp = cn + "=";
-            let dc = decodeURIComponent(document.cookie);
-            let ca = dc.split(';');
-
-            for(let i = 0; i < ca.length; i++)
-            {
-                let chr = ca[i];
-
-                while (chr.charAt(0) == String.fromCharCode(32)) chr = chr.substring(1);
-
-                if (chr) return chr.substring(cn.length, c.length);
-            }
-
-        }
-
-        return null;
-    }
-
 })(similarityframe);
+
+function setCookie(cn, cv, ex = 0)
+{
+    if (cn && cv)
+    {
+        let dt = new Date();
+        dt.setTime(dt.getTime() + (ex * 24 * 60 * 60 * 1000));
+        let expires = "expires=" + dt.toUTCString();
+
+        document.cookie = cn + "=" + cv + ";" + expires + ";path=/";
+    }
+
+}
+
+function getCookie(cn)
+{
+    if (cn)
+    {
+        let cp = cn + "=";
+        let dc = decodeURIComponent(document.cookie);
+        let ca = dc.split(';');
+
+        for(let i = 0; i < ca.length; i++)
+        {
+            let chr = ca[i];
+
+            while (chr.charAt(0) == String.fromCharCode(32)) chr = chr.substring(1);
+
+            if (chr) return chr.substring(cn.length, c.length);
+        }
+
+    }
+
+    return null;
+}
