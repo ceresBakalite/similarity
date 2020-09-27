d0c8c45006b8e9fcff6b05c096b475264e2cee5e
ceresbakalite <ceres@users.noreply.github.com>
2020-09-28 02:18:27 +1000

    Update similarityscriptframe.js

--- repos/scripts/similarityscriptframe.js
+++ repos/scripts/similarityscriptframe.js
@@ -95,12 +95,12 @@ let similarityframe = {};
             if (el.style.display && window.scrollY > trigger)
             {
                 if (el.style.display != 'none') setTimeout(function(){ setStyleDisplay('none'); }, 250);
-                window.parent.document.setCookie('hd', true);
+                window.parent.setCookie('hd', true);
 
             } else {
 
                 if (el.style.display != 'block') setTimeout(function(){ setStyleDisplay('block'); }, 250);
-                window.parent.document.setCookie('hd', false);
+                window.parent.setCookie('hd', false);
 
             }
 
