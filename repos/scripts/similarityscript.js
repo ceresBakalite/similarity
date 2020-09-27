7d33aa44cbf0cb9fe27345e617b2023245d456c3
ceresbakalite <ceres@users.noreply.github.com>
2020-09-28 02:54:28 +1000

    Update similarityscript.js

--- repos/scripts/similarityscript.js
+++ repos/scripts/similarityscript.js
@@ -14,8 +14,6 @@ let similarity = {};
     ceres.onloadPrimary = function() { onloadPrimary(); }; // public method reference
     ceres.getMarkupDocument = function(id, el) { getMarkupDocument(id, el); };  // public method reference
     ceres.resetPinState = function(el) { resetPinState(el); };  // public method reference
-    ceres.setCookie = function(cn, cv, ex) { setCookie(cn, cv, ex = 0); };  // public method reference
-    ceres.getCookie = function(cn) { getCookie(cn); };  // public method reference
 
     class Component
     {
@@ -91,39 +89,39 @@ let similarity = {};
 
     }
 
-    function setCookie(cn, cv, ex = 0)
-    {
-        if (cn && cv)
-        {
-            let dt = new Date();
-            dt.setTime(dt.getTime() + (ex * 24 * 60 * 60 * 1000));
-            let expires = "expires=" + dt.toUTCString();
-
-            document.cookie = cn + "=" + cv + ";" + expires + ";path=/";
-        }
-
-    }
-
-    function getCookie(cn)
-    {
-        if (cn)
-        {
-            let cp = cn + "=";
-            let dc = decodeURIComponent(document.cookie);
-            let ca = dc.split(';');
-
-            for(let i = 0; i < ca.length; i++)
-            {
-                let chr = ca[i];
-
-                while (chr.charAt(0) == String.fromCharCode(32)) chr = chr.substring(1);
-
-                if (chr) return chr.substring(cn.length, c.length);
-            }
-
-        }
-
-        return null;
-    }
-
 })(similarity);
+
+function setCookie(cn, cv, ex = 0)
+{
+    if (cn && cv)
+    {
+        let dt = new Date();
+        dt.setTime(dt.getTime() + (ex * 24 * 60 * 60 * 1000));
+        let expires = "expires=" + dt.toUTCString();
+
+        document.cookie = cn + "=" + cv + ";" + expires + ";path=/";
+    }
+
+}
+
+function getCookie(cn)
+{
+    if (cn)
+    {
+        let cp = cn + "=";
+        let dc = decodeURIComponent(document.cookie);
+        let ca = dc.split(';');
+
+        for(let i = 0; i < ca.length; i++)
+        {
+            let chr = ca[i];
+
+            while (chr.charAt(0) == String.fromCharCode(32)) chr = chr.substring(1);
+
+            if (chr) return chr.substring(cn.length, c.length);
+        }
+
+    }
+
+    return null;
+}
