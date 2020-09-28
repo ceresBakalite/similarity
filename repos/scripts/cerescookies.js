72994e584a1694804991739b69e4b7f224f7951c
ceresbakalite <ceres@users.noreply.github.com>
2020-09-29 01:44:53 +1000

    Update cerescookies.js

--- repos/scripts/cerescookies.js
+++ repos/scripts/cerescookies.js
@@ -1,25 +1,22 @@
 function setCookie(name, value, options = {})
 {
-    // Example of use:
     //setCookie('user', 'John', {secure: true, 'max-age': 3600});
 
     options = {
         path: '/',
-        // add other defaults here if necessary
-        //...options
     };
 
     if (options.expires instanceof Date) { options.expires = options.expires.toUTCString(); }
 
-    let updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);
+    let updatedCookie = encodeURIComponent(name) + '=' + encodeURIComponent(value);
 
     for (let optionKey in options)
     {
-        updatedCookie += "; " + optionKey;
+        updatedCookie += '; ' + optionKey;
         let optionValue = options[optionKey];
         if (optionValue !== true)
         {
-            updatedCookie += "=" + optionValue;
+            updatedCookie += '=' + optionValue;
         }
 
     }
@@ -27,7 +24,7 @@ function setCookie(name, value, options = {})
     document.cookie = updatedCookie;
 }
 
-function getCookie(name) 
+function getCookie(name)
 {
   let matches = document.cookie.match(new RegExp("(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"));
   return matches ? decodeURIComponent(matches[1]) : undefined;
