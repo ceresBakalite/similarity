0583a7ecf18bdd3bdaa99762aef8492ad8742fac
ceresbakalite <ceres@users.noreply.github.com>
2020-09-29 03:05:37 +1000

    Update cerescookies.js

--- repos/scripts/cerescookies.js
+++ repos/scripts/cerescookies.js
@@ -1,72 +1,31 @@
-class Cookies {
+function setCookie(name, value, options = {})
+{
+    //setCookie('user', 'John', {secure: true, 'max-age': 3600});
 
-    constructor(name, value, options = {})
+    options = {
+        path: '/',
+    };
+
+    if (options.expires instanceof Date) { options.expires = options.expires.toUTCString(); }
+
+    let updatedCookie = encodeURIComponent(name) + '=' + encodeURIComponent(value);
+
+    for (let optionKey in options)
     {
-        this.name = name;
-        this.value = value;
-        this.options = options;
-    }
-
-    set()
-    {
-        this.options = {
-            path: '/',
-        };
-
-        if (this.options.expires instanceof Date) { this.options.expires = this.options.expires.toUTCString(); }
-
-        let updatedCookie = encodeURIComponent(this.name) + '=' + encodeURIComponent(this.value);
-
-        for (let optionKey in this.options)
+        updatedCookie += '; ' + optionKey;
+        let optionValue = options[optionKey];
+        if (optionValue !== true)
         {
-            updatedCookie += '; ' + optionKey;
-            let optionValue = this.options[optionKey];
-            if (optionValue !== true)
-            {
-                updatedCookie += '=' + optionValue;
-            }
-
+            updatedCookie += '=' + optionValue;
         }
 
-        document.cookie = updatedCookie;
     }
 
-    get()
-    {
-        let matches = document.cookie.match(new RegExp("(?:^|; )" + this.name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"));
-        return matches ? decodeURIComponent(matches[1]) : undefined;
-    }
+    document.cookie = updatedCookie;
+}
 
-    function xxxsetCookie(name, value, options = {})
-    {
-        //setCookie('user', 'John', {secure: true, 'max-age': 3600});
-
-        options = {
-            path: '/',
-        };
-
-        if (options.expires instanceof Date) { options.expires = options.expires.toUTCString(); }
-
-        let updatedCookie = encodeURIComponent(name) + '=' + encodeURIComponent(value);
-
-        for (let optionKey in options)
-        {
-            updatedCookie += '; ' + optionKey;
-            let optionValue = options[optionKey];
-            if (optionValue !== true)
-            {
-                updatedCookie += '=' + optionValue;
-            }
-
-        }
-
-        document.cookie = updatedCookie;
-    }
-
-    function xxxgetCookie(name)
-    {
-        let matches = document.cookie.match(new RegExp("(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"));
-        return matches ? decodeURIComponent(matches[1]) : undefined;
-    }
-
+function getCookie(name)
+{
+  let matches = document.cookie.match(new RegExp("(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
 }
