f5524bacfb103d28af087021be5a2ee1003b6806
ceresbakalite <ceres@users.noreply.github.com>
2020-09-29 16:18:58 +1000

    Update similarityscriptframe.js

--- repos/scripts/similarityscriptframe.js
+++ repos/scripts/similarityscriptframe.js
@@ -98,7 +98,7 @@ let similarityframe = {};
                 {
                     setTimeout(function(){ setStyleDisplay('none'); }, 250);
                     //window.parent.setCookie('hd', true, {'max-age': 3600});
-                    window.parent.cookies.setCookie('hd', true, {'max-age': 3600});
+                    window.parent.cookies.setCookie('hd', true);
                 }
 
             } else {
@@ -107,7 +107,7 @@ let similarityframe = {};
                 {
                     setTimeout(function(){ setStyleDisplay('block'); }, 250);
                     //window.parent.setCookie('hd', false, {'max-age': 3600});
-                    window.parent.cookies.setCookie('hd', false, {'max-age': 3600});
+                    window.parent.cookies.setCookie('hd', false);
                 }
 
             }
