b3f8c6d9171bd147d0064e03a390cb0b321ae480
ceresbakalite <ceres@users.noreply.github.com>
2020-09-30 01:48:31 +1000

    Update cerescookies.js

--- repos/scripts/cerescookies.js
+++ repos/scripts/cerescookies.js
@@ -42,4 +42,24 @@ var myApp = {};
       return matches ? decodeURIComponent(matches[1]) : undefined;
     }
 
+    this.set = function (name, value, options = {})
+    {
+        options = {
+            path: '/',
+        };
+
+        if (options.expires instanceof Date) { options.expires = options.expires.toUTCString(); }
+
+        let updatedCookie = encodeURIComponent(name) + '=' + encodeURIComponent(value);
+
+        for (let optionKey in options)
+        {
+            updatedCookie += '; ' + optionKey;
+            let optionValue = options[optionKey];
+            if (optionValue !== true) updatedCookie += '=' + optionValue;
+        }
+
+        document.cookie = updatedCookie;
+    }
+
 }).apply(myApp);
