a556791782b4953e102b7deeb080c5c42595335f
ceresbakalite <ceres@users.noreply.github.com>
2020-10-02 02:12:12 +1000

    Update cerescookies.js

--- repos/scripts/cerescookies.js
+++ repos/scripts/cerescookies.js
@@ -20,8 +20,9 @@ var cookies = {};
         for (let item in options)
         {
             cookie += '; ' + item;
-            let option = options[item];
-            if (option) cookie += '=' + option;
+            //let option = options[item];
+            //if (option) cookie += '=' + option;
+            cookie = (options[item]) ? cookie += '=' + options[item] : cookie;
         }
 
         document.cookie = cookie;
