754070c11dae099e4f0ebd354842831ebe250597
ceresbakalite <ceres@users.noreply.github.com>
2020-10-04 04:54:47 +1100

    Update similarityscript.js

--- repos/scripts/similarityscript.js
+++ repos/scripts/similarityscript.js
@@ -79,7 +79,7 @@ var similarity = {};
 
     function resetPinState(el)
     {
-        //test.sayhi('Sandy');
+        modtest.sayhi('Sandy');
 
         if (el.getAttribute('state') == 'enabled')
         {
