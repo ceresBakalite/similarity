287ffff962484fd0616099f2b40b2472cd1441fa
ceresbakalite <ceres@users.noreply.github.com>
2020-10-03 22:11:57 +1000

    Update similarityscriptframe.js

--- repos/scripts/similarityscriptframe.js
+++ repos/scripts/similarityscriptframe.js
@@ -105,7 +105,7 @@ var similarityframe = {};
 
         function setStyleDisplay(attribute)
         {
-            window.parent.cookies.set('hd', attribute, { 'max-age': 3600 });
+            window.parent.cookies.set('hd', attribute, { 'max-age': 7200 });
             el.style.display = attribute;
         }
 
