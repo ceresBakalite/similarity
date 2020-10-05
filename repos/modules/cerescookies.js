ebf60a4a952524aff8f9a53fd55b422d06832398
ceresbakalite <ceres@users.noreply.github.com>
2020-10-06 04:26:15 +1100

    Update cerescookies.js

--- repos/modules/cerescookies.js
+++ repos/modules/cerescookies.js
@@ -24,38 +24,6 @@ var cookies = {};
         document.cookie = cookie;
     }
 
+    window.onload = similarity.onload();
+    
 }).apply(cookies);
-
-
-/*
-var test = {};
-(function() {
-
-    this.sayhi = function (name)
-    {
-        console.log('hi ' + name);
-    }
-
-    this.get = function (name)
-    {
-        let match = document.cookie.match(new RegExp("(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"));
-        return match ? decodeURIComponent(match[1]) : undefined;
-    }
-
-    this.set = function (name, value, options = {})
-    {
-        let cookie = encodeURIComponent(name) + '=' + encodeURIComponent(value);
-
-        if (!options.path) options.path = '/';
-        if (options.expires instanceof Date) { options.expires = options.expires.toUTCString(); }
-
-        for (let item in options)
-        {
-            cookie += '; ' + item + '=' + ((typeof options[item] != null) ? options[item] : null);
-        }
-
-        document.cookie = cookie;
-    }
-
-}).apply(test);
-*/
