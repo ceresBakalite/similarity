3ac12165d6031767d25e762f11f2cf3ca295ffd9
ceresbakalite <ceres@users.noreply.github.com>
2020-10-05 22:02:17 +1100

    Update cerescookies.js

--- repos/modules/cerescookies.js
+++ repos/modules/cerescookies.js
@@ -1,3 +1,33 @@
+export { cookies }
+
+var cookies = {};
+(function() {
+
+    this.get = function (name)
+    {
+        let match = document.cookie.match(new RegExp("(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"));
+        return match ? decodeURIComponent(match[1]) : undefined;
+    }
+
+    this.set = function (name, value, options = {})
+    {
+        let cookie = encodeURIComponent(name) + '=' + encodeURIComponent(value);
+
+        if (!options.path) options.path = '/';
+        if (options.expires instanceof Date) { options.expires = options.expires.toUTCString(); }
+
+        for (let item in options)
+        {
+            cookie += '; ' + item + '=' + ((typeof options[item] != null) ? options[item] : null);
+        }
+
+        document.cookie = cookie;
+    }
+
+}).apply(cookies);
+
+
+/*
 var test = {};
 (function() {
 
@@ -28,5 +58,4 @@ var test = {};
     }
 
 }).apply(test);
-
-export default test;
+*/
