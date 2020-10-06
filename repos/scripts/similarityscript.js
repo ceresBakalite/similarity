cfdf7c8e48d4a79b6eefb4232f4ccf3f12039f9a
ceresbakalite <ceres@users.noreply.github.com>
2020-10-06 21:17:05 +1100

    Update similarityscript.js

--- repos/scripts/similarityscript.js
+++ repos/scripts/similarityscript.js
@@ -55,11 +55,11 @@ var similarity = {};
 
     function getHeaderAttributes()
     {
-        if (!cookies.get('hd')) cookies.set('hd', 'block', { 'max-age': 7200 });
-        if (!cookies.get('pn')) cookies.set('pn', 'disabled', { 'max-age': 7200 });
+        if (!window.cookies.get('hd')) window.cookies.set('hd', 'block', { 'max-age': 7200 });
+        if (!window.cookies.get('pn')) window.cookies.set('pn', 'disabled', { 'max-age': 7200 });
 
-        if (cookies.get('hd') == 'none') document.getElementById('site-header-display').style.display = 'none';
-        if (cookies.get('pn') == 'enabled') setPinState(document.getElementById('pin-navbar'), 'enabled');
+        if (window.cookies.get('hd') == 'none') document.getElementById('site-header-display').style.display = 'none';
+        if (window.cookies.get('pn') == 'enabled') setPinState(document.getElementById('pin-navbar'), 'enabled');
     }
 
     function onloadPrimary()
@@ -100,14 +100,14 @@ var similarity = {};
     {
         let header = document.getElementById('site-header-display');
         if (header.style.display != 'block') setTimeout(function() { header.style.display = 'block'; }, 250);
-        cookies.set('hd', attribute, { 'max-age': 7200 });
+        window.cookies.set('hd', attribute, { 'max-age': 7200 });
     }
 
     function setPinState(el, attribute)
     {
         el.src = pinimage.get(attribute);
         el.setAttribute('state', attribute);
-        cookies.set('pn', attribute, { 'max-age': 7200 });
+        window.cookies.set('pn', attribute, { 'max-age': 7200 });
     }
 
 })(similarity);
