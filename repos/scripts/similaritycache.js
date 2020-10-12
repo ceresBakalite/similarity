0656f88ce35f23190214e47e859cb00b0f98ce3a
ceresbakalite <ceres@users.noreply.github.com>
2020-10-12 23:40:34 +1100

    cache

--- repos/scripts/similaritycache.js
+++ repos/scripts/similaritycache.js
@@ -12,13 +12,20 @@ var similaritycache = {};
             {
               return cache.addAll([
                 'https://ceresbakalite.github.io/similarity/index.html',
-                'https://ceresbakalite.github.io/similarity/stylesheets/stylesheet.css',
+                'https://ceresbakalite.github.io/similarity/repos/scripts/SyncSlide.html',
+                'https://ceresbakalite.github.io/similarity/repos/scripts/SyncSlide.html',
+                'https://ceresbakalite.github.io/similarity/repos/scripts/SyncSlide.html',
+                'https://ceresbakalite.github.io/similarity/repos/scripts/SyncIndex.html',
+                'https://fonts.googleapis.com/css?family=Open+Sans:400,700',
+                'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css',
+                'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css',
                 'https://ceresbakalite.github.io/similarity/stylesheets/github-light.css',
                 'https://ceresbakalite.github.io/similarity/stylesheets/normalize.css',
+                'https://ceresbakalite.github.io/similarity/stylesheets/stylesheet.css',
                 'https://ceresbakalite.github.io/similarity/stylesheets/similarityIndex.css',
-                'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css',
                 'https://ceresbakalite.github.io/similarity/repos/scripts/similarityscriptframe.js',
                 'https://ceresbakalite.github.io/similarity/repos/scripts/similarityscript.js',
+                'https://ceresbakalite.github.io/similarity/repos/scripts/cerescookies.js',
                 'https://ceresbakalite.github.io/similarity/images/slide/NAVScreenViews01.png',
                 'https://ceresbakalite.github.io/similarity/images/slide/NAVScreenViews02.png',
                 'https://ceresbakalite.github.io/similarity/images/slide/NAVScreenViews03.png',
@@ -26,7 +33,14 @@ var similaritycache = {};
                 'https://ceresbakalite.github.io/similarity/images/slide/NAVScreenViews05.png',
                 'https://ceresbakalite.github.io/similarity/images/slide/NAVScreenViews06.png',
                 'https://ceresbakalite.github.io/similarity/images/slide/NAVScreenViews07.png',
-                'https://ceresbakalite.github.io/similarity/images/slide/NAVScreenViews08.png'
+                'https://ceresbakalite.github.io/similarity/images/slide/NAVScreenViews08.png',
+                'https://ceresbakalite.github.io/similarity/images/NAVBridgeViewHeaderBackground.png',
+                'https://ceresbakalite.github.io/similarity/images/NAVSimilarityLogoScripts.png',
+                'https://ceresbakalite.github.io/similarity/images/GitHubForkMe_Right_Transparent.png',
+                'https://ceresbakalite.github.io/similarity/images/NAVSimilarityLogoScripts.png',
+                'https://ceresbakalite.github.io/similarity/images/NAVLampPeriscope.png',
+                'https://ceresbakalite.github.io/similarity/images/NAVLampRing.png',
+                'https://ceresbakalite.github.io/similarity/images/NAVLampBulb.png'
               ]);
 
             })
