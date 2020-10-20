27fad38f10f7cf424c9fcea89ea507d258c40f23
ceresbakalite <ceres@users.noreply.github.com>
2020-10-20 23:05:00 +1100

    test ceresb

--- repos/scripts/similarityscript.js
+++ repos/scripts/similarityscript.js
@@ -3,26 +3,6 @@ export { similarity }
 import { cookies } from 'https://ceresbakalite.github.io/similarity/repos/scripts/cerescookies.js';
 import { similaritycache } from 'https://ceresbakalite.github.io/similarity/repos/scripts/similaritycache.js';
 
-const frame = document.getElementById('frame-container');
-frame.contentWindow.postMessage('test', 'http://ceresb.com');
-
-window.addEventListener('message', event =>
-{
-    // IMPORTANT: check the origin of the data!
-    if (event.origin.startsWith('https://ceresbakalite.github.io'))
-    {
-        // The data was sent from your site.
-        // Data sent with postMessage is stored in event.data:
-        console.log(event.data);
-    } else {
-        // The data was NOT sent from your site!
-        // Be careful! Do not use it. This else branch is
-        // here just for clarity, you usually shouldn't need it.
-        return;
-    }
-});
-
-
 var similarity = {};
 (function()
 {
