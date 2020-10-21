bd6d5adc7c44e037b3da78385d97cdc606bc0e69
ceresbakalite <ceres@users.noreply.github.com>
2020-10-21 13:33:34 +1100

    Update similarityscriptframe.js

--- repos/scripts/similarityscriptframe.js
+++ repos/scripts/similarityscriptframe.js
@@ -1,6 +1,6 @@
 export { similarityframe }
 
-import { cookies } from './repos/scripts/cerescookies.js';
+import { cookies } from '/repos/scripts/cerescookies.js';
 
 var similarityframe = {};
 (function()
