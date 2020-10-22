b8b96ff56309174fba08adb6ef4c705b25f65fef
ceresbakalite <ceres@users.noreply.github.com>
2020-10-22 19:06:06 +1100

    Update cerescookies.js

--- repos/scripts/cerescookies.js
+++ repos/scripts/cerescookies.js
@@ -14,7 +14,7 @@ var cookies = {};
         let cookie = encodeURIComponent(name) + '=' + encodeURIComponent(value);
 
         if (!options.path) options.path = '/';
-        if (!options.SameSite) options.SameSite = 'SameSite': 'Lax; Secure';
+        if (!options.SameSite) options.SameSite = 'Lax; Secure';
         if (options.expires instanceof Date) { options.expires = options.expires.toUTCString(); }
 
         for (let item in options)
