a74fce588076d80d21d90382cb04e5156478310d
ceresbakalite <ceres@users.noreply.github.com>
2020-10-22 12:01:25 +1100

    Update similaritycache.js

--- repos/scripts/similaritycache.js
+++ repos/scripts/similaritycache.js
@@ -5,6 +5,12 @@ var similaritycache = {};
 {
     'use strict';
 
+    // for all responses
+    app.use((req, res, next) => {
+      res.set('Cache-Control', 'public, max-age 2592000');
+      next();
+    });
+    
     if ('caches' in window)
     {
         window.addEventListener('install', function(event)
