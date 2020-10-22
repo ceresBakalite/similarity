001b9ee4db6fecbceb85fd641257c52c440106e3
ceresbakalite <ceres@users.noreply.github.com>
2020-10-22 14:12:03 +1100

    Update similaritycache.js

--- repos/scripts/similaritycache.js
+++ repos/scripts/similaritycache.js
@@ -5,60 +5,83 @@ var similaritycache = {};
 {
     'use strict';
 
+    let cacheName = 'similarity-cache';
+    let deleteCache = false; // manual override only
+
     if ('caches' in window)
     {
+
+        if (deleteCache)
+        {
+            caches.keys().then(function(cacheNames)
+            {
+                return Promise.all(
+                    cacheNames.map(function(cacheName)
+                    {
+                        if(cacheName != CACHE_NAME)
+                        {
+                            return caches.delete(cacheName);
+                        }
+                    })
+
+                );
+
+            });
+
+        }
+
         window.addEventListener('install', function(e)
         {
           e.waitUntil(
-            caches.open('similarity-cache').then( function(cache)
+            caches.open(cacheName).then( function(cache)
             {
               return cache.addAll([
                   './index.html',
                   './README.md',
-                './repos/scripts/SyncIndex.html',
-                './repos/scripts/SyncRepos.html',
-                './repos/scripts/SyncShell.html',
-                './repos/scripts/SyncSlide.html',
-                './repos/scripts/IncludeNavigation.inc',
-                './repos/scripts/IncludeHeader.inc',
-                './repos/scripts/IncludeFooter.inc',
-                './repos/scripts/index.md',
-                './repos/scripts/similarityscriptframe.js',
-                './repos/scripts/similaritycache.js',
-                './repos/scripts/similarityscript.js',
-                './repos/scripts/cerescookies.js',
-                './stylesheets/stylesheets/github-light.css',
-                './stylesheets/stylesheets/normalize.css',
-                './stylesheets/stylesheets/stylesheet.css',
-                './stylesheets/stylesheets/similarityIndex.css',
-                './images/slide/NAVScreenViews01.png',
-                './images/slide/NAVScreenViews02.png',
-                './images/slide/NAVScreenViews03.png',
-                './images/slide/NAVScreenViews04.png',
-                './images/slide/NAVScreenViews05.png',
-                './images/slide/NAVScreenViews06.png',
-                './images/slide/NAVScreenViews07.png',
-                './images/slide/NAVScreenViews08.png',
-                './images/slide/README.md',
-                './images/GitHubForkMe_Right_Transparent.png',
-                './images/NAVBridgeViewHeaderBackground.png',
-                './images/NAVSimilarityLogoRepos.png',
-                './images/NAVSimilarityLogoScripts.png',
-                './images/NAVSimilarityLogoShell.png',
-                './images/NAVSimilarityLogoStyle.png',
-                './images/AppleTouchIcon.png',
-                './images/NAVPinIconDisabled.png',
-                './images/NAVPinIconEnabled.png',
-                './images/NAVLampPeriscope.png',
-                './images/NAVLampHalf.png',
-                './images/NAVLampRing.png',
-                './images/NAVLampBulb.png',
-                './images/NAVCreate.png',
-                './images/NAVCogs.png',
-                'https://ceresbakalite.github.io/ceres-sv/prod/ceres-sv.min.js',
-                'https://ceresbakalite.github.io/ceres-sv/prod/ceres-sv.lib.min.js',
-                'https://fonts.googleapis.com/css?family=Open+Sans:400,700',
-                'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css'
+                  './repos/scripts/SyncIndex.html',
+                  './repos/scripts/SyncRepos.html',
+                  './repos/scripts/SyncShell.html',
+                  './repos/scripts/SyncSlide.html',
+                  './repos/scripts/IncludeNavigation.inc',
+                  './repos/scripts/IncludeHeader.inc',
+                  './repos/scripts/IncludeFooter.inc',
+                  './repos/scripts/index.md',
+                  './repos/scripts/similarityscriptframe.js',
+                  './repos/scripts/similaritycache.js',
+                  './repos/scripts/similarityscript.js',
+                  './repos/scripts/cerescookies.js',
+                  './stylesheets/stylesheets/github-light.css',
+                  './stylesheets/stylesheets/normalize.css',
+                  './stylesheets/stylesheets/stylesheet.css',
+                  './stylesheets/stylesheets/similarityIndex.css',
+                  './images/slide/NAVScreenViews01.png',
+                  './images/slide/NAVScreenViews02.png',
+                  './images/slide/NAVScreenViews03.png',
+                  './images/slide/NAVScreenViews04.png',
+                  './images/slide/NAVScreenViews05.png',
+                  './images/slide/NAVScreenViews06.png',
+                  './images/slide/NAVScreenViews07.png',
+                  './images/slide/NAVScreenViews08.png',
+                  './images/slide/README.md',
+                  './images/GitHubForkMe_Right_Transparent.png',
+                  './images/NAVBridgeViewHeaderBackground.png',
+                  './images/NAVSimilarityLogoRepos.png',
+                  './images/NAVSimilarityLogoScripts.png',
+                  './images/NAVSimilarityLogoShell.png',
+                  './images/NAVSimilarityLogoStyle.png',
+                  './images/AppleTouchIcon.png',
+                  './images/NAVPinIconDisabled.png',
+                  './images/NAVPinIconEnabled.png',
+                  './images/NAVLampPeriscope.png',
+                  './images/NAVLampHalf.png',
+                  './images/NAVLampRing.png',
+                  './images/NAVLampBulb.png',
+                  './images/NAVCreate.png',
+                  './images/NAVCogs.png',
+                  'https://ceresbakalite.github.io/ceres-sv/prod/ceres-sv.min.js',
+                  'https://ceresbakalite.github.io/ceres-sv/prod/ceres-sv.lib.min.js',
+                  'https://fonts.googleapis.com/css?family=Open+Sans:400,700',
+                  'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css'
               ]);
 
             })
@@ -87,7 +110,7 @@ var similaritycache = {};
                         // and serve second one
                         let responseClone = response.clone();
 
-                        caches.open('similarity-cache').then(function (cache)
+                        caches.open(cacheName).then(function (cache)
                         {
                             responseClone.set('Cache-Control', 'public, max-age 604800, s-maxage 43200');
                             cache.put(e.request, responseClone);
