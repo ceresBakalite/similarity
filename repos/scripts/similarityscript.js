8a1271b5a7d28cc948104b073948197b8aea61b8
ceresbakalite <ceres@users.noreply.github.com>
2020-10-24 00:11:45 +1100

    Update similarityscript.js

--- repos/scripts/similarityscript.js
+++ repos/scripts/similarityscript.js
@@ -2,7 +2,7 @@ export { similarity }
 
 import { cookies } from '/../repos/mods/cerescookies.js';
 import { cereslibrary as csl} from '/../repos/mods/cereslibrary.js';
-import { similaritycache } from './similaritycache.js';
+import { similaritycache } from '/../repos/mods/similaritycache.js';
 
 var similarity = {};
 (function()
