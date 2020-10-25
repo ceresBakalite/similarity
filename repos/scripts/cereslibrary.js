1a4686cea41fe1ff69181d6f271fb01c803be9af
ceresbakalite <ceres@users.noreply.github.com>
2020-10-25 16:14:50 +1100

    Update cereslibrary.js

--- repos/scripts/cereslibrary.js
+++ repos/scripts/cereslibrary.js
@@ -9,9 +9,9 @@
  *
  * Copyright (c) 2020 Alexander Munro
 */
-export { resource, cookies }
+export { generic, touch, compose, cookies }
 
-var resource = {};
+var generic = {};
 (function()
 {
     'use strict';
@@ -38,74 +38,6 @@ var resource = {};
     this.isString = function(obj) { return Object.prototype.toString.call(obj) == '[object String]'; }
     this.clearElement = function(el) { while (el.firstChild) el.removeChild(el.firstChild); }
 
-    this.setHorizontalSwipe = function(touch, callback, args)
-    {
-        const el = document.querySelector(touch.el);
-
-        if (!touch.act) touch.act = 10;
-
-        el.addEventListener('touchstart', e => { touch.start = e.changedTouches[0].screenX; }, { passive: true } );
-
-        el.addEventListener('touchmove', e => { e.preventDefault(); }, { passive: true });
-
-        el.addEventListener('touchend', e =>
-        {
-            touch.end = e.changedTouches[0].screenX;
-
-            if (Math.abs(touch.start - touch.end) > touch.act)
-            {
-                args.action = (touch.start > touch.end);
-                callback.call(this, args);
-            }
-
-        }, { passive: true });
-
-    }
-
-    this.composeElement = function(element)
-    {
-        const el = document.createElement(element.el);
-
-        el.id = element.id;
-        element.parent.appendChild(el);
-
-        if (element.classValue) this.composeAttribute({ id: el.id, type: 'class', value: element.classValue });
-        if (element.onClickEvent) this.composeAttribute({ id: el.id, type: 'onclick', value: element.onClickEvent });
-        if (element.url) this.composeAttribute({ id: el.id, type: 'src', value: element.url });
-        if (element.accessibility) this.composeAttribute({ id: el.id, type: 'alt', value: element.accessibility });
-        if (element.markup) document.getElementById(el.id).insertAdjacentHTML('afterbegin', element.markup);
-    }
-
-    this.composeAttribute = function(attribute)
-    {
-        const el = document.getElementById(attribute.id);
-
-        if (el)
-        {
-            const attributeNode = document.createAttribute(attribute.type);
-            attributeNode.value = attribute.value;
-
-            el.setAttributeNode(attributeNode);
-        }
-
-    }
-
-    this.composeLinkElement = function(attribute)
-    {
-        const link = document.createElement('link');
-
-        if (attribute.rel) link.rel = attribute.rel;
-        if (attribute.type) link.type = attribute.type;
-        if (attribute.href) link.href = attribute.href;
-        if (attribute.as) link.as = attribute.as;
-        if (attribute.crossorigin) link.crossorigin = attribute.crossorigin;
-        if (attribute.media) link.media = attribute.media;
-
-        link.addEventListener('load', function() {}, false);
-
-        document.head.appendChild(link);
-    }
-
     this.isEmptyOrNull = function(obj)
     {
         if (obj === null || obj == 'undefined') return true;
@@ -126,16 +58,6 @@ var resource = {};
         const token = attribute.trim().toLowerCase();
 
         return sbl.has(token) ? sbl.get(token) : false;
-    }
-
-    this.getUniqueElementId = function(str = null, range = 100)
-    {
-        let elName = function() { return str + Math.floor(Math.random() * range) };
-        let el = null;
-
-        while (document.getElementById(el = elName())) {};
-
-        return el;
     }
 
     this.inspect = function(diagnostic)
@@ -191,7 +113,95 @@ var resource = {};
         Object.freeze(rsc);
     }
 
-}).call(resource);
+}).call(generic);
+
+var compose = {};
+(function() {
+
+    this.composeElement = function(element)
+    {
+        const el = document.createElement(element.el);
+
+        el.id = element.id;
+        element.parent.appendChild(el);
+
+        if (element.classValue) this.composeAttribute({ id: el.id, type: 'class', value: element.classValue });
+        if (element.onClickEvent) this.composeAttribute({ id: el.id, type: 'onclick', value: element.onClickEvent });
+        if (element.url) this.composeAttribute({ id: el.id, type: 'src', value: element.url });
+        if (element.accessibility) this.composeAttribute({ id: el.id, type: 'alt', value: element.accessibility });
+        if (element.markup) document.getElementById(el.id).insertAdjacentHTML('afterbegin', element.markup);
+    }
+
+    this.composeAttribute = function(attribute)
+    {
+        const el = document.getElementById(attribute.id);
+
+        if (el)
+        {
+            const attributeNode = document.createAttribute(attribute.type);
+            attributeNode.value = attribute.value;
+
+            el.setAttributeNode(attributeNode);
+        }
+
+    }
+
+    this.composeLinkElement = function(attribute)
+    {
+        const link = document.createElement('link');
+
+        if (attribute.rel) link.rel = attribute.rel;
+        if (attribute.type) link.type = attribute.type;
+        if (attribute.href) link.href = attribute.href;
+        if (attribute.as) link.as = attribute.as;
+        if (attribute.crossorigin) link.crossorigin = attribute.crossorigin;
+        if (attribute.media) link.media = attribute.media;
+
+        link.addEventListener('load', function() {}, false);
+
+        document.head.appendChild(link);
+    }
+
+    this.composeElementId = function(str = null, range = 100)
+    {
+        let elName = function() { return str + Math.floor(Math.random() * range) };
+        let el = null;
+
+        while (document.getElementById(el = elName())) {};
+
+        return el;
+    }
+
+}).call(compose);
+
+var touch = {};
+(function() {
+
+    this.setHorizontalSwipe = function(touch, callback, args)
+    {
+        const el = document.querySelector(touch.el);
+
+        if (!touch.act) touch.act = 10;
+
+        el.addEventListener('touchstart', e => { touch.start = e.changedTouches[0].screenX; }, { passive: true } );
+
+        el.addEventListener('touchmove', e => { e.preventDefault(); }, { passive: true });
+
+        el.addEventListener('touchend', e =>
+        {
+            touch.end = e.changedTouches[0].screenX;
+
+            if (Math.abs(touch.start - touch.end) > touch.act)
+            {
+                args.action = (touch.start > touch.end);
+                callback.call(this, args);
+            }
+
+        }, { passive: true });
+
+    }
+
+}).call(touch);
 
 var cookies = {};
 (function() {
