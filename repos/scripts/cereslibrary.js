f50680e3f7f25b62810a545aea8a1b11b74793c5
ceresbakalite <ceres@users.noreply.github.com>
2020-11-07 17:06:04 +1100

    Update cereslibrary.js

--- repos/scripts/cereslibrary.js
+++ repos/scripts/cereslibrary.js
@@ -212,7 +212,7 @@ var compose = {};
     {
         const nodelist = document.querySelectorAll(el.node); // shadowroot markdown node - ie zero-md or ceres-sv
 
-        if (!el.regex) el.regex = /<a (?!target)/gm;
+        if (!el.regex) el.regex = /<a (?!target)/gmi;
         if (!el.replacement) el.replacement = '<a target="_top" ';
 
         nodelist.forEach(node => {
