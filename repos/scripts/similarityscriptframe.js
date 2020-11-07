5b3f715cddd80b32b45edcb3ca8464baa00c29b1
ceresbakalite <ceres@users.noreply.github.com>
2020-11-07 22:14:39 +1100

    Update similarityscriptframe.js

--- repos/scripts/similarityscriptframe.js
+++ repos/scripts/similarityscriptframe.js
@@ -33,15 +33,21 @@ var similarityframe = {};
         const pin = parent.document.querySelector('img.pin-navbar').getAttribute('state');
         const trigger = 25;
 
+        const setStyleDisplay = function(attribute)
+        {
+            cookies.set('hd', attribute, { 'max-age': 7200, 'samesite': 'None; Secure' });
+            header.style.display = attribute;
+        }
+
         if (pin == 'disabled')
         {
             if (header.style.display && window.scrollY > trigger)
             {
-                if (header.style.display != 'none') setTimeout(function(){ rsc.setStyleDisplay('none'); }, 250);
+                if (header.style.display != 'none') setTimeout(function(){ setStyleDisplay('none'); }, 250);
 
             } else {
 
-                if (header.style.display != 'block') setTimeout(function(){ rsc.setStyleDisplay('block'); }, 250);
+                if (header.style.display != 'block') setTimeout(function(){ setStyleDisplay('block'); }, 250);
             }
 
         }
@@ -56,12 +62,6 @@ var similarityframe = {};
             let displayFooter = function()
             {
                 setTimeout(function() {  document.querySelector('div.footer-content').style.display = 'block'; }, 2000);
-            }
-
-            rsc.setStyleDisplay = function(attribute)
-            {
-                cookies.set('hd', attribute, { 'max-age': 7200, 'samesite': 'None; Secure' });
-                header.style.display = attribute;
             }
 
             rsc.isValidSource = function()
@@ -84,6 +84,7 @@ var similarityframe = {};
             }
 
         })(); // end resource allocation
+
 
     }
 
