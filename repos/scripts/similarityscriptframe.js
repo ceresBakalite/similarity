9f5f31078bf0c4889a4a491fb725b1738dd5b65f
ceresbakalite <ceres@users.noreply.github.com>
2020-11-23 16:29:53 +1100

    Update similarityscriptframe.js

--- repos/scripts/similarityscriptframe.js
+++ repos/scripts/similarityscriptframe.js
@@ -21,46 +21,42 @@ var similarityframe = {};
         {
             rsc.invokeScrollEventListener();
             rsc.asyncPullMarkdownRequest();
+            rsc.displaySlideviewContent();
         }
 
-    }
-
-    function adjustHeaderDisplay()
-    {
-        const header = parent.document.querySelector('div.page-header');
-        const pin = parent.document.querySelector('img.pin-navbar').getAttribute('state');
-        const trigger = 25;
-
-        const setStyleDisplay = function(attribute)
-        {
-            cookies.set('hd', attribute, { 'max-age': 7200, 'samesite': 'None; Secure' });
-            header.style.display = attribute;
-        }
-
-        if (pin == 'disabled')
-        {
-            if (header.style.display && window.scrollY > trigger)
-            {
-                if (header.style.display != 'none') setTimeout(function(){ setStyleDisplay('none'); }, 250);
-
-            } else {
-
-                if (header.style.display != 'block') setTimeout(function(){ setStyleDisplay('block'); }, 250);
-            }
-
-        }
-
-    }
-
-    function displayFooter()
-    {
-        setTimeout(function() {  document.querySelector('div.footer-content').style.display = 'block'; }, 2000);
     }
 
     function initialise()
     {
         ceres.rsc = {};
         (function() {
+
+            rsc.adjustHeaderDisplay = function()
+            {
+                const header = parent.document.querySelector('div.page-header');
+                const pin = parent.document.querySelector('img.pin-navbar').getAttribute('state');
+                const trigger = 25;
+
+                const setStyleDisplay = function(attribute)
+                {
+                    cookies.set('hd', attribute, { 'max-age': 7200, 'samesite': 'None; Secure' });
+                    header.style.display = attribute;
+                }
+
+                if (pin == 'disabled')
+                {
+                    if (header.style.display && window.scrollY > trigger)
+                    {
+                        if (header.style.display != 'none') setTimeout(function(){ setStyleDisplay('none'); }, 250);
+
+                    } else {
+
+                        if (header.style.display != 'block') setTimeout(function(){ setStyleDisplay('block'); }, 250);
+                    }
+
+                }
+
+            }
 
             rsc.isValidSource = function()
             {
@@ -80,7 +76,13 @@ var similarityframe = {};
             rsc.asyncPullMarkdownRequest = function()
             {
                 setTimeout(function() { resource.composeCORSLinks( { node: 'zero-md', query: 'div.markdown-body' } ); }, 1000);
-                displayFooter();
+                setTimeout(function() {  document.querySelector('div.footer-content').style.display = 'block'; }, 2000);
+            }
+
+            rsc.displaySlideviewContent = function()
+            {
+                let el = document.querySelector('slideview-content none');
+                if (el) el.className = 'slideview-content'
             }
 
         })(); // end resource allocation
