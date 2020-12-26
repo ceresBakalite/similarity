a254e880dd561f6e42a09f1664e0498320f7359c
ceresbakalite <ceres@users.noreply.github.com>
2020-12-26 16:26:19 +1100

    Update similarityscriptframe.js

--- repos/scripts/similarityscriptframe.js
+++ repos/scripts/similarityscriptframe.js
@@ -2,7 +2,7 @@ export { similarityframe };
 
 import { include, cookies, resource } from '../mods/cereslibrary.min.js';
 
-globalThis.testCall = (el) => {
+var testCall = (el) => {
 
     console.log('hello 1 from testCall: ' + el.src);
 }
