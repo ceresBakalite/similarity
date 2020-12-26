b6605161b3d8a6db7e6a88ae43fcec66e7d248f8
ceresbakalite <ceres@users.noreply.github.com>
2020-12-26 19:56:18 +1100

    Update cereslibrary.js

--- repos/scripts/cereslibrary.js
+++ repos/scripts/cereslibrary.js
@@ -76,7 +76,7 @@ var resource = {};
             if (shadow) {
 
                 let shard = shadow.querySelector(el.query);
-                shard.innerHTML = this.softSanitize(shard.innerHTML.replace(el.regex, el.replace));
+                shard.innerHTML = shard.innerHTML.replace(el.regex, el.replace).replace(/</g, '&lt;');
             }
 
         });
