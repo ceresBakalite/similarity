c8f861845470170a11e3ff020118842c975419c1
ceresbakalite <ceres@users.noreply.github.com>
2020-12-26 19:43:04 +1100

    Update cereslibrary.js

--- repos/scripts/cereslibrary.js
+++ repos/scripts/cereslibrary.js
@@ -76,7 +76,7 @@ var resource = {};
             if (shadow) {
 
                 let shard = shadow.querySelector(el.query);
-                shard.innerHTML = shard.innerHTML.replace(el.regex, el.replace);
+                shard.replaceWith(shard.innerHTML.replace(el.regex, el.replace));
             }
 
         });
