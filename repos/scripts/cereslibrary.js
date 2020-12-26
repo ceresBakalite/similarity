0a5f25e3018f9483b1716e0d58f576d2234ae337
ceresbakalite <ceres@users.noreply.github.com>
2020-12-26 18:13:41 +1100

    Update cereslibrary.js

--- repos/scripts/cereslibrary.js
+++ repos/scripts/cereslibrary.js
@@ -75,8 +75,10 @@ var resource = {};
 
             if (shadow) {
 
-                let markdown = shadow.querySelector(el.query).innerHTML; // the content we wish to alter
-                shadow.querySelector(el.query).innerHTML = markdown.replace(el.regex, el.replace);
+                let markdown = shadow.querySelector(el.query).textContent; // the content we wish to alter
+                shadow.querySelector(el.query).textContent = markdown.replace(el.regex, el.replace);
+                //let markdown = shadow.querySelector(el.query).innerHTML; // the content we wish to alter
+                //shadow.querySelector(el.query).innerHTML = markdown.replace(el.regex, el.replace);
             }
 
         });
