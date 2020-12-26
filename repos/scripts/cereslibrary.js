94761cf3f017e27c774220f28a1f1fc934365c87
ceresbakalite <ceres@users.noreply.github.com>
2020-12-26 21:50:47 +1100

    Update cereslibrary.js

--- repos/scripts/cereslibrary.js
+++ repos/scripts/cereslibrary.js
@@ -76,7 +76,7 @@ var resource = {};
             if (shadow) {
 
                 let shard = shadow.querySelector(el.query);
-                let markdown = shard.nodeValue; // the html content we wish to alter
+                let markdown = shard.firstChild.nodeValue; // the html content we wish to alter
 
                 this.clearElement(shard);
                 shard.insertAdjacentHTML('afterbegin', markdown.replace(el.regex, el.replace));
